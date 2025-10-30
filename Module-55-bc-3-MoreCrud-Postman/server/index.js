94ba64293dd3e0677f2a112f519ab8585e89974a
ehasun15019 <ehasunulislam0202@gmail.com>
2025-10-31 00:41:42 +0600

    module 5 time 4.30min

--- Module-55-bc-3-MoreCrud-Postman/server/index.js
+++ Module-55-bc-3-MoreCrud-Postman/server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 // import from mongodb clusters
-const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -29,75 +29,76 @@ async function run() {
     await client.connect();
 
     /* create collection start */
-    const productDB = client.db('product_DB');
-    const productsCollection = productDB.collection('products')
+    const productDB = client.db("product_DB");
+    const productsCollection = productDB.collection("products");
     /* create collection end */
 
 
     /* call a get method for getting all data */
     app.get("/products", async (req, res) => {
+      // const projectFields = {title: 1, price_min: 1, price_max: 1, image: 1}
+
+      // const cursor = productsCollection.find().sort({
+      //   price_min: 1
+      // }).skip(2).limit(3).project(projectFields);
+
       const cursor = productsCollection.find();
+
       const result = await cursor.toArray();
       res.send(result);
-    })
-
+    });
 
     /* call a get method for single data */
     app.get("/products/:id", async (req, res) => {
       const productID = req.params.id;
       const query = {
-        _id: new ObjectId(productID)
-      }
+        _id: new ObjectId(productID),
+      };
 
       const result = await productsCollection.findOne(query);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     /* call a post method for sending data in mongoDB */
     app.post("/products", async (req, res) => {
       const newProducts = req.body;
       const result = await productsCollection.insertOne(newProducts);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     /* call a patch method for updating data */
     app.patch("/products/:id", async (req, res) => {
       const productID = req.params.id;
       const query = {
-        _id: new ObjectId(productID)
-      }
+        _id: new ObjectId(productID),
+      };
 
       const updateProducts = req.body;
 
       const update = {
         $set: {
           name: updateProducts.name,
-          price: updateProducts.price
-        }
-      }
+          price: updateProducts.price,
+        },
+      };
 
       const result = await productsCollection.updateOne(query, update);
       res.send(result);
-    })
-
+    });
 
     /* delete method functionality */
     app.delete("/products/:id", async (req, res) => {
       const productID = req.params.id;
       const query = {
-        _id: new ObjectId(productID)
-      }
-      const result = await productsCollection.deleteOne(query)
-      res.send(result)
-    })
-
+        _id: new ObjectId(productID),
+      };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Ping success MongoDB");
   } finally {
-
   }
 }
 
