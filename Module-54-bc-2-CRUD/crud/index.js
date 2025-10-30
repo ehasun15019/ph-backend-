e7053e57606308c50f43e55a9356ff95958e5ca2
ehasun15019 <ehasunulislam0202@gmail.com>
2025-10-30 19:49:55 +0600

    update

--- Module-54-bc-2-CRUD/crud/index.js
+++ Module-54-bc-2-CRUD/crud/index.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 // mongoDB uri
-// const uri = "mongodb+srv://simpleDBUser:CnoErlcaT5W4STuH@ic-cluster.qdhi4wp.mongodb.net/?appName=ic-cluster";
-const uri = "mongodb+srv://simpleDBUser:CnoErlcaT5W4STuH@ic-cluster.qdhi4wp.mongodb.net/?appName=ic-cluster";
+const uri = "mongodb+srv://phModule:RoSNFHAorcljxV98@ic-cluster.qdhi4wp.mongodb.net/?appName=ic-cluster";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
