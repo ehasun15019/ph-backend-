9d66df8d00697b5dd0120b6303ec0febaab84f53
ehasun15019 <ehasunulislam0202@gmail.com>
2025-10-30 23:04:32 +0600

    create collection in backend

--- Module-55-bc-3-MoreCrud-Postman/server/index.js
+++ Module-55-bc-3-MoreCrud-Postman/server/index.js
@@ -1,36 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const cors = require('cors');
+const cors = require("cors");
 
 // import from mongodb clusters
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
 // mongodb uri
-const uri = "mongodb+srv://phModule55:bWNzs56kHLuFLwzX@ic-cluster.qdhi4wp.mongodb.net/?appName=ic-cluster";
+const uri =
+  "mongodb+srv://phModule55:bWNzs56kHLuFLwzX@ic-cluster.qdhi4wp.mongodb.net/?appName=ic-cluster";
 
-// import from mongodb clusters
+// coming from mongodb clusters start
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
+async function run() {
+  try {
+    await client.connect();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    /* create collection start */
+    const productDB = client.db('product_DB');
+    const productsCollection = productDB.collection('products')
+    /* create collection end */
 
+    await client.db("admin").command({ ping: 1 });
+    console.log("Ping success MongoDB");
+  } finally {
+
+  }
+}
+
+run().catch(console.dir);
+// coming from mongodb clusters end
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 // this function is calling for mongodb
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`server is running on port http://localhost:${port}`);
+});
